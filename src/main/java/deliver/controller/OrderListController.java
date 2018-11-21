bf49bdeff7d02d63173a7b2c2f41c6268bcc5740
Baisalbek Daniiarov <43205440+xbaysal11@users.noreply.github.com>
2018-11-21 17:07:12 +0600

    implemented method

--- src/main/java/deliver/controller/OrderListController.java
+++ src/main/java/deliver/controller/OrderListController.java
@@ -1,16 +1,49 @@
 package deliver.controller;
 
+import deliver.model.*;
+
+import java.util.List;
+
 public class OrderListController {
+    public static deliver.database.controllers.DB_Manager db_manager = new deliver.database.controllers.DB_Manager();
+    private Object Order;
+    private Order order = new Order();
 
-    private void showOpenOrderList(){
 
+    //Open orders list!
+    private void showOpenOrderList() {
+        db_manager.findAll((Class) Order);
+        String status = order.getStatus();
+        int id = order.getId();
+        String oName = order.getOrderName();
+        String from = order.getFrom();
+        String destination = order.getDestination();
+        String note = order.getNote();
+        int personId = order.getPerson_id();
+        if (status == "Open") {
+            System.out.println("Delivering ID: " + id + ". Order name: "+oName+". Status: "+status+". Person ID: "+personId+". From: "+from+". To: "+destination+". Note: " + note+".");
+        }
     }
 
-    private void showDoneOrderList(){
-
+    //Done orderes list!
+    private void showDoneOrderList() {
+        db_manager.findAll((Class) Order);
+        String status = order.getStatus();
+        int id = order.getId();
+        String oName = order.getOrderName();
+        String from = order.getFrom();
+        String destination = order.getDestination();
+        String note = order.getNote();
+        int personId = order.getPerson_id();
+        if (status == "Done") {
+            System.out.println("Delivering ID: " + id + ". Order name: "+oName+". Status: "+status+". Person ID: "+personId+". From: "+from+". To: "+destination+". Note: " + note+".");
+        }
     }
 
-    private void showAllOrderList(){
+    //All orders list!
+    private void showAllOrderList() {
+        List all = db_manager.findAll((Class) Order);
+        System.out.println(all);
 
     }
 
