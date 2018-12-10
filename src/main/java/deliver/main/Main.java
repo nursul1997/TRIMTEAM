784e5c24bd4dceb450d70b7b0746adc8fb15bab7
NurtilekZ <nzakonbekov@gmail.com>
2018-12-10 13:19:39 +0600

    FXML and Maven bug fix

--- src/main/java/deliver/main/Main.java
+++ src/main/java/deliver/main/Main.java
@@ -5,7 +5,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -14,9 +13,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("deliver/view/mainBoard.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/mainBoard.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
